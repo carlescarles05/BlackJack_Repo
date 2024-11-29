7d5464abe30643a809aabde4af72f54af4f30e4b
Luchi37 <lucianocarrionzipitria@gmail.com>
2024-11-29 12:52:50 +0100

    BlackJackCanvasMesaArreglar

---
+++ PruebasIntegrado3DLOL/Assets/blackjack-card-game-master/blackjack-card-game-master/src/unity/Card Game/Assets/Scripts/CardModel.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class CardModel : MonoBehaviour
+{
+    private SpriteRenderer _renderer;
+    private CardFlipper flipper;
+    public Sprite[] faces;
+    public Sprite cardBack;
+    public int cardIndex; // e.g. faces[cardIndex];
+
+    public void ToggleFace(bool showFace)
+    {
+        if (showFace)
+        {
+            flipper.FlipCard(faces[cardIndex - 1], faces[cardIndex], cardIndex);
+        }
+        else
+        {
+            flipper.FlipCard(cardBack, faces[cardIndex], cardIndex);
+        }
+    }
+
+    public void ToggleFaceNoAnimation(bool showFace)
+    {
+        if (showFace)
+        {
+            _renderer.sprite = faces[cardIndex];
+        }
+        else
+        {
+            _renderer.sprite = cardBack;
+        }
+    }
+
+    void Awake()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+        flipper = GetComponent<CardFlipper>();
+    }
+}
