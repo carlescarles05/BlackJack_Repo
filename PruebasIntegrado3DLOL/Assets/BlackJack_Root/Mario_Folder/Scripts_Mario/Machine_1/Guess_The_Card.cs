077e47587c00606e5c252de65b573008cb0b3614
Mari0Pineda <mariocallejas19@gmail.com>
2024-12-17 13:20:44 +0100

    Scene_machine1
    changes

--- PruebasIntegrado3DLOL/Assets/BlackJack_Root/Mario_Folder/Scripts_Mario/Machine_1/Guess_The_Card.cs
+++ PruebasIntegrado3DLOL/Assets/BlackJack_Root/Mario_Folder/Scripts_Mario/Machine_1/Guess_The_Card.cs
@@ -1,43 +1,32 @@
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.InputSystem;
-using Unity.VisualScripting;
-
-
 
 public class GuessTheCard : MonoBehaviour
 {
-    //DInput
     public GameObject[] cards;
     public Text resultText;
+    public Text winText;
     private int MachineNumber;
     public Player_Points player_Points;
     public Player_Clock player_Clock;
-    private int selectedCardIndexPos = 0;
+
+    private int selectedCardIndexPos = 0; // Selected card index
+    private int turnCount = 0;            // New: Turn counter
+
     private GameInputActions inputActions;
-
     private Vector2 navigationInput;
-
-/// ////////////////
-/// ///////
-/// 
 
     void Start()
     {
         StartGame();
     }
-    void StartGame()
-    {
-        //INIT
-        MachineNumber = Random.Range(1, 14); // carda between 1 and 13 selection.
-        HighLightCard(selectedCardIndexPos,true);
-        resultText.text = "Elige una carta.";
-        Debug.Log($"Machine has picked card number: {MachineNumber}");
-    }
-    //< Updated upstream
-    void Awake()
 
+    void Awake()
     {
         //KEYS INPUT
         inputActions = new GameInputActions();
@@ -46,14 +35,15 @@ public class GuessTheCard : MonoBehaviour
         inputActions.Navigate.Submit.performed += OnSubmit;
         inputActions.Navigate.Enable();  //Enable the Navigate action map
     }
-    void Update()
-    {
-       
-    }
 
-    /// <summary>
-    /// ///////////// Method screen navigation
-    /// </summary>
+    void StartGame()
+    {
+        //INIT
+        MachineNumber = Random.Range(1, 14); // Cards between 1 and 13 selection
+        ResetCardHighlightByTurn();
+        resultText.text = "Elige una carta.";
+        Debug.Log($"Machine has picked card number: {MachineNumber}");
+    }
 
     void OnNavigate(InputAction.CallbackContext context)
     {
@@ -68,81 +58,61 @@ public class GuessTheCard : MonoBehaviour
         }
     }
 
-    // method when key down
     void OnSubmit(InputAction.CallbackContext context)
     {
         Debug.Log($"Submitted! Selected Index: {selectedCardIndexPos}");
         SelectedCardAction(cards[selectedCardIndexPos]);
     }
 
-    /// <summary>
-    /// /////////////
-    /// </summary>
-
-    void MoveSelection(int direction) 
+    void MoveSelection(int direction)
     {
         HighLightCard(selectedCardIndexPos, false);
 
-        //update the index
+        // Update the index
         selectedCardIndexPos += direction;
 
         if (selectedCardIndexPos >= cards.Length)
         {
-
             selectedCardIndexPos = 0;
-
         }
         else if (selectedCardIndexPos < 0)
         {
-            selectedCardIndexPos = cards.Length-1;
+            selectedCardIndexPos = cards.Length - 1;
         }
 
         HighLightCard(selectedCardIndexPos, true);
     }
 
-    ////////////////
-    /// ////////
-    ////
-    void HighLightCard(int index, bool highlight) 
+    void HighLightCard(int index, bool highlight)
     {
-     Renderer renderer = cards[index].GetComponent<Renderer>();
-        if (highlight) 
+        Renderer renderer = cards[index].GetComponent<Renderer>();
+        if (highlight)
         {
             renderer.material.color = Color.yellow;
         }
         else
         {
-         renderer.material.color =Color.white;
+            renderer.material.color = Color.white;
         }
     }
 
-    //////////////
-    ////////
-    ////
-    ///
-    void HighlightMachineCard()  //Apart
+    void HighlightMachineCard()
     {
-        foreach (var card in cards) 
+        foreach (var card in cards)
         {
             int cardNumber = int.Parse(card.name.Replace("Card_", ""));
             if (cardNumber == MachineNumber)
             {
-             Renderer renderer = card.GetComponent<Renderer>();
-                renderer.material.color = Color.red; //machine card highlight
+                Renderer renderer = card.GetComponent<Renderer>();
+                renderer.material.color = Color.red; // machine card highlight
                 break;
             }
         }
     }
-    //////////////
-    ////////
-    ////
-    ///
 
     void SelectedCardAction(GameObject clickedCard)
     {
-        // Get the card's assigned number
         int cardNumber = int.Parse(clickedCard.name.Replace("Card_", ""));
-
         int difference = Mathf.Abs(cardNumber - MachineNumber);
 
         if (difference == 0)
@@ -150,54 +120,72 @@ public class GuessTheCard : MonoBehaviour
             player_Points.AddPoints(+250);
             player_Clock.AddTime(8 * 60);
             resultText.text = $"¡Adivinaste la carta! Tienes +8 minutos de vida. Reiniciando...";
-            
         }
         else if (difference == 1)
         {
             player_Points.AddPoints(+200);
             player_Clock.AddTime(5 * 60);
             resultText.text = $"¡Casi aciertas! Tienes +5 minutos.";
-          
         }
         else if (difference == 2)
         {
             resultText.text = "No recibes nada esta vez.";
-          
         }
         else if (difference <= 5)
         {
             player_Points.AddPoints(-200);
             player_Clock.AddTime(-2 * 60);
             resultText.text = $"Por poco. Pierdes tiempo (-2 minutos).";
-        
         }
         else if (difference <= 8)
         {
             player_Points.AddPoints(-30);
             player_Clock.AddTime(-5 * 60);
             resultText.text = $"Estás algo lejos. Pierdes tiempo (-5 minutos).";
-         
         }
         else
         {
             player_Points.AddPoints(-100);
             player_Clock.AddTime(-8 * 60);
             resultText.text = $"Te alejaste demasiado. Pierdes tiempo (-8 minutos).";
-           
-
-        
         }
+
         HighlightMachineCard();
+        turnCount++; // Increment the turn counter
         Invoke("RestartGame", 2f);
     }
+
     void RestartGame()
     {
         foreach (GameObject card in cards)
         {
-        
             Renderer renderer = card.GetComponent<Renderer>();
             renderer.material.color = Color.white;
         }
+
         StartGame(); // Reinitialize the game state
+    }
+
+    /// <summary>
+    /// Resets the card highlight position based on the turn count.
+    /// </summary>
+    void ResetCardHighlightByTurn()
+    {
+        turnCount %= 3; // Cycle every 3 turns
+
+        if (turnCount == 0) // First turn: Highlight first card
+        {
+            selectedCardIndexPos = 0;
+        }
+        else if (turnCount == 1) // Second turn: Highlight middle card
+        {
+            selectedCardIndexPos = 4; //cards.Length / 2;
+        }
+        else if (turnCount == 2) // Third turn: Highlight last card
+        {
+            selectedCardIndexPos = cards.Length - 1;
+        }
+
+        HighLightCard(selectedCardIndexPos, true);
     }
 }
--- PruebasIntegrado3DLOL/Assets/BlackJack_Root/Mario_Folder/Scripts_Mario/Public/Buttons.cs
+++ PruebasIntegrado3DLOL/Assets/BlackJack_Root/Mario_Folder/Scripts_Mario/Public/Buttons.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Buttons : MonoBehaviour
 {
     public GameObject MenuCanvas;
     public GameObject Game1Canvas;
+    public GameObject cardDeck;
     // Start is called before the first frame update
     public void SwitchToMENUCanvas() 
     {
-        Game1Canvas.SetActive(false);
+        Game1Canvas.gameObject.SetActive(false);
         MenuCanvas.SetActive(true);
+        cardDeck.gameObject.SetActive(false); 
     }
 
     // Update is called once per frame
