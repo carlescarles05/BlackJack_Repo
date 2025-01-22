0b33a8aebd6dc7d9d0d061729808a4a609f72628
carlescarles05 <151786310+carlescarles05@users.noreply.github.com>
2025-01-22 10:03:25 +0100

    menu opciones en proceso

---
+++ PruebasIntegrado3DLOL/Assets/BlackJack_Root/Scripts/MenuPause.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPause : MonoBehaviour
+{
+    [SerializeField] private GameObject botonPausa;
+    [SerializeField] private GameObject menuPausa;
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        botonPausa.SetActive(false);
+        menuPausa.SetActive(true);
+    }
+
+    public void Reanudar()
+    {
+        Time.timeScale = 1f;
+        botonPausa.SetActive(true);
+        menuPausa.SetActive(false);
+    }
+}
